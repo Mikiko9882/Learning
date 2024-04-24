b28fad9bf3c54ad7d9e5ec8de9ac4ce3e9338f89
Mikiko9882 <teddy.0802.t@gmail.com>
2024-04-24 16:46:34 +0900

    school作成まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_065342) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_071722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_065342) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "email"
+    t.string "website"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_schools_on_code", unique: true
   end
 
   create_table "teachers", force: :cascade do |t|
