a7e03bbe99c64bac5e682d9f0acf8fc4d5bba934
Mikiko9882 <teddy.0802.t@gmail.com>
2024-04-25 19:32:58 +0900

    調整

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -20,6 +20,10 @@ class Admins::SessionsController < Devise::SessionsController
 
   protected
 
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     new_admin_admin_session_path
   end
--- app/controllers/teacher/base_controller.rb
+++ app/controllers/teacher/base_controller.rb
@@ -1,2 +1,5 @@
 class Teacher::BaseController < ApplicationController
+  def after_sign_in_path_for(resource)
+    teacher_root_path
+  end
 end
